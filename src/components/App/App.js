ea7c3631252643e04390690f62d377ff93da46a3
mes32 <stockman.mike@gmail.com>
2019-12-18 08:59:56 -0600

    Setup client-side routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 import './App.css';
+
+import CartPage from '../CartPage/CartPage';
+import Navbar from '../Navbar/Navbar';
+import SearchPage from '../SearchPage/SearchPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Game Filter</h1>
+      <Navbar />
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="/search" />
+          <Route exact path="/search" component={SearchPage} />
+          <Route exact path="/cart" component={CartPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/SearchPage/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchPage() {
+    return (
+        <div>
+            <p>[ Search Page ]</p>
+        </div>
+    );
+}
+
+export default SearchPage;
