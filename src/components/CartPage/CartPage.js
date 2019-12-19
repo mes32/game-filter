15e9d2123643b181daa9927a9ecec9268b01379c
mes32 <stockman.mike@gmail.com>
2019-12-19 14:24:32 -0600

    Add a checkout button to cart page (just deletes all items from the cart)

--- src/components/CartPage/CartPage.js
+++ src/components/CartPage/CartPage.js
@@ -3,23 +3,30 @@ import { connect } from 'react-redux';
 import CartEntry from '../CartEntryRow/CartEntryRow';
 import './CartPage.css';
 
-function CartPage({ cart }) {
+function CartPage({ cart, dispatch }) {
+    const checkout = () => {
+        dispatch({ type: 'CLEAR_CART_ITEMS' });
+    }
+
     return (
         <div>
             <h1>Shopping Cart</h1>
             {cart.length === 0 ? <p>[ Your cart is empty ]</p> :
-                <table className="cart-table">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Game Title</th>
-                            <th>Remove</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {cart.map((game, i) => <CartEntry key={i} game={game} index={i} />)}
-                    </tbody>
-                </table>
+                <>
+                    <button onClick={checkout}>Checkout (i.e. delete all)</button>
+                    <table className="cart-table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Game Title</th>
+                                <th>Remove</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cart.map((game, i) => <CartEntry key={i} game={game} index={i} />)}
+                        </tbody>
+                    </table>
+                </>
             }
         </div>
     );
